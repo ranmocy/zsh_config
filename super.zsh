bf9ec325dd3b7873b264c075f20a470187e909d6
Ranmocy Sheng <ranmocy@gmail.com>
2015-11-09 03:36:01 +0800

    use ruby everywhere

--- super.zsh
+++ super.zsh
@@ -35,26 +35,16 @@ setopt AUTO_PUSHD #启用 cd 命令的历史纪录，cd -[TAB]进入历史路径
 setopt PUSHD_IGNORE_DUPS #相同的历史路径只保留一个
 setopt HIST_IGNORE_SPACE #在命令前添加空格，不将此命令添加到纪录文件中
 
+# Boost Ruby
+export RUBY_GC_HEAP_INIT_SLOTS=1000000 # 1M
+export RUBY_GC_HEAP_FREE_SLOTS=500000  # 0.5M
+export RUBY_GC_HEAP_GROWTH_FACTOR=1.1
+export RUBY_GC_HEAP_GROWTH_MAX_SLOTS=10000000 # 10M
+export RUBY_GC_MALLOC_LIMIT_MAX=1000000000    # 1G
+export RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR=1.1
 
-if [[ `whoami` == "ranmocy" ]]; then
-    # GitCafe ENV
-    # export RAILS_ENV="development"
-    export GITCAFE_SERVER="localhost"
-    export HTTP_OR_HTTPS="http"
-    export MONGOID_HOST="localhost"
-    export MONGOID_DATABASE="git_cafe_development"
-
-    # Boost Ruby
-    export RUBY_GC_HEAP_INIT_SLOTS=1000000 # 1M
-    export RUBY_GC_HEAP_FREE_SLOTS=500000  # 0.5M
-    export RUBY_GC_HEAP_GROWTH_FACTOR=1.1
-    export RUBY_GC_HEAP_GROWTH_MAX_SLOTS=10000000 # 10M
-    export RUBY_GC_MALLOC_LIMIT_MAX=1000000000    # 1G
-    export RUBY_GC_MALLOC_LIMIT_GROWTH_FACTOR=1.1
-
-    export PATH=$PATH:$HOME/.rvm/bin
-    [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-fi
+export PATH=$PATH:$HOME/.rvm/bin
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # Highlight command
 source $HOME/.zshrc.d/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh

bc13b2de48a80ad6d2027a34b1928a2bb0e72c01
Ranmocy Sheng <ranmocy@gmail.com>
2017-07-06 13:50:51 -0700

    add notify support for linux

--- custom/plugins/notify/notify.plugin.zsh
+++ custom/plugins/notify/notify.plugin.zsh
@@ -21,11 +21,26 @@ function _notify-terminal-notifier() {
   terminal-notifier $args >/dev/null 2>&1
 }
 
+function _notify-notify-send() {
+  # -u, --urgency=LEVEL               Specifies the urgency level (low, normal, critical).
+  # -t, --expire-time=TIME            Specifies the timeout in milliseconds at which to expire the notification.
+  # -a, --app-name=APP_NAME           Specifies the app name for the icon
+  # -i, --icon=ICON[,ICON...]         Specifies an icon filename or stock icon to display.
+  # -c, --category=TYPE[,TYPE...]     Specifies the notification category.
+  # -h, --hint=TYPE:NAME:VALUE        Specifies basic extra data to pass. Valid types are int, double, string and byte.
+  notify-send --urgency=normal --expire-time=1000 --app-name="$2" -- "$1" >/dev/null 2>&1
+}
+
 alias notify=_notify-echo
 case `uname -s` in
   Darwin)
     if [[ $(whence terminal-notifier) != "" ]]; then
       alias notify=_notify-terminal-notifier
+    fi
+    ;;
+  Linux)
+    if [[ $(whence notify-send) != "" ]]; then
+      alias notify=_notify-notify-send
     fi
     ;;
 esac
